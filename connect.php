b033b61c77e27a7b9634ec5593659b5acd15e00a
Luc1l1us <ikazuha001@gmail.com>
2023-01-13 01:29:46 +0800

    finished backed

--- connect.php
+++ connect.php
@@ -1,104 +1,220 @@
 <?php
-
-// past version 
-/*if (isset($_POST['STDB'])) {
-    if (isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['middleinitial']) && isset($_POST['gender']) && isset($_POST['emailadd']) && isset($_POST['civstatus']) && isset($_POST['mobilenumb']) && isset($_POST['birth']) && isset($_POST['shippingadd']) && isset($_POST['ccnum']) && isset($_POST['dateexpire']) && isset($_POST['threedigit'])) {
-
-        $lastname = $_POST['lastname'];
-        $firstname = $_POST['firstname']; 
-        $middleinitial = $_POST['middleinitial'];
-        $gender = $_POST['gender'];
-        $emailadd = $_POST['emailadd'];
-        $civstatus = $_POST['civstatus'];
-        $mobilenumb = $_POST['mobilenumb'];
-        $birth = $_POST['birth'];
-        $shippingadd = $_POST['shippingadd'];
-        $ccnum = $_POST['ccnum'];
-        $dateexpire = $_POST['dateexpire'];
-        $threedigit = $_POST['threedigit'];*/
-              
-            $con = mysqli_connect('localhost','root', '', 'lhuile');
-            if (mysqli_connect_error()) {
+    mysqli_report(MYSQLI_REPORT_ALL);            
+    $con = mysqli_connect('localhost','root', '', 'martin');
+    if (mysqli_connect_error()) {
                 exit('Connection Failed : '.mysqli_connect_error());
-            } 
-
-            if ($stmt = $con->prepare('SELECT emailadd From test Where emailadd = ? Limit 1')) {
-                $stmt->bind_param('s', $_POST['emailadd']);
-                $stmt->execute();
-                $stmt->store_result();
-
-                if ($stmt->num_rows>0) {
-                    echo 'Email Address already taken. Try again.';
-                } else {
-                    if ($stmt = $con->prepare("INSERT INTO test (firstname, lastname, middleinitial, gender, emailadd, civstatus, mobilenumb, birth, shippingadd, ccnum, dateexpire, threedigit) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
-
-                        /*$lastname = $_POST['lastname'];
-                        $firstname = $_POST['firstname']; 
-                        $middleinitial = $_POST['middleinitial'];
-                        $gender = $_POST['gender'];
-                        $emailadd = $_POST['emailadd'];
-                        $civstatus = $_POST['civstatus'];
-                        $mobilenumb = $_POST['mobilenumb'];
-                        $birth = $_POST['birth'];
-                        $shippingadd = $_POST['shippingadd'];
-                        $ccnum = $_POST['ccnum'];
-                        $dateexpire = $_POST['dateexpire'];
-                        $threedigit = $_POST['threedigit'];*/
+    } 
+    if ($stmt = $con->prepare("INSERT INTO martindanrev (firstname, lastname, middleinitial, gender, emailadd, civstatus, mobilenumb, birth, shippingadd, ccnum, dateexpire, threedigit) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) 
                         
-                        $stmt->bind_param("ssssssissisi", $_POST['firstname'], $_POST['lastname'], $_POST['middleinitial'], $_POST['gender'], $_POST['emailadd'], $_POST['civstatus'], $_POST['mobilenumb'], $_POST['birth'], $_POST['shippingadd'], $_POST['ccnum'], $_POST['dateexpire'], $_POST['threedigit']);
-                        $stmt->execute();
-                        echo 'Succcessfully Registed';
+    $stmt->bind_param("sssssssssisi", $_POST['firstname'], $_POST['lastname'], $_POST['middleinitial'], $_POST['gender'], $_POST['emailadd'], $_POST['civstatus'], $_POST['mobilenumb'], $_POST['birth'], $_POST['shippingadd'], $_POST['ccnum'], $_POST['dateexpire'], $_POST['threedigit']);
+    $stmt->execute();
+    $con->close();
 
-                    } 
-                    else {
-                        echo 'Error Occured with inputting values';
-                    }
-                }
-            } else {
-                echo 'Error Occured in preparing values';
-            }
-
-            $con->close();
-
-
-                /*$stmt = $conn->prepare("Insert into register(firstname, lastname, middleinitial, gender, emailadd, civstatus, mobilenumb, birth, shippingadd, ccnum, dateexpire, threedigit) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-                $stmt->bind_param("ssssssissisi", $firstname, $lastname, $middleinitial, $gender, $emailadd, $civstatus, $mobilenumb, $birth, $shippingadd, $ccnum, $dateexpire, $threedigit);
-                $stmt->execute();
-                echo "Registered Successfully...";
-                $stmt->close();
-                $conn->close();
-            
-
-
-                // past version 
-                $Select = "SELECT emailadd From register Where emailadd = ? Limit 1";
-                $Insert = "INSERT INTO register (firstname, lastname, middleinitial, gender, emailadd, civstatus, mobilenumb, birth, shippingadd, ccnum, dateexpire, threedigit) values (?,?,?,?,?,?,?,?,?,?,?,?)";
-                
-                $stmt = $conn->prepare($SELECT);
-                $stmt->bind_param("s", $emailadd);
-                $stmt->execute();
-                $stmt->bind_result($emailadd);
-                $stmt->store_result();
-                $stmt->fetch();
-                $rnum = $stmt->num_rows;
-                
-                if ($rnum == 0) {
-                    $stmt->close();
-                    
-                    $stmt = $conn->prepare($Insert);
-                    $stmt->bind_param("ssssssissisi", $firstname, $lastname, $middleinitial, $gender, $emailadd, $civstatus, $mobilenumb, $birth, $shippingadd, $ccnum, $dateexpire, $threedigit);
-                    if ($stmt->execute()) {
-                        echo "New record inserted successfully";
-                    } else {
-                        echo $stmt->error;
-                    }
-                    
-                } else {
-                    echo "Someone already registered using this email";
-                }
-                $stmt->close();
-                $conn->close();
-            }
-        }
-    }*/
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<!--DELETE-->
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
+
+	<!-- title -->
+	<title>404!</title>
+
+	<!-- favicon -->
+	<link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
+	<!-- google font -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+	<!-- fontawesome -->
+	<link rel="stylesheet" href="assets/css/all.min.css">
+	<!-- bootstrap -->
+	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+	<!-- owl carousel -->
+	<link rel="stylesheet" href="assets/css/owl.carousel.css">
+	<!-- magnific popup -->
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<!-- animate css -->
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<!-- mean menu css -->
+	<link rel="stylesheet" href="assets/css/meanmenu.min.css">
+	<!-- main style -->
+	<link rel="stylesheet" href="assets/css/main.css">
+	<!-- responsive -->
+	<link rel="stylesheet" href="assets/css/responsive.css">
+
+</head>
+<body>
+	
+	<!--PreLoader-->
+    <div class="loader">
+        <div class="loader-inner">
+            <div class="circle"></div>
+        </div>
+    </div>
+    <!--PreLoader Ends-->
+	
+	<!-- header -->
+	<div class="top-header-area" id="sticker">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-sm-12 text-center">
+					<div class="main-menu-wrap">
+						<!-- logo -->
+						<div class="site-logo">
+							<a href="index.html">
+								<img src="assets/img/logo.png" alt="">
+							</a>
+						</div>
+						<!-- logo -->
+
+						<!-- menu start -->
+						<nav class="main-menu">
+							<ul class="nav justify-content-end">
+								<li><a href="index.html">Home</a></li>
+								<li><a href="shop-pg1.html">Shop</a></li>
+							<li><a href="about.html">About</a></li>
+								<li><a href="register.html">Register</a></li>
+								<li>
+								</li>
+							</ul>
+						</nav>
+						<!-- menu end -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end header -->
+
+		<!-- breadcrumb-section -->
+		<div class="breadcrumb-section breadcrumb-bg">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-8 offset-lg-2 text-center">
+						<div class="breadcrumb-text">
+							<p>Register</p>
+							<h1>Thank You!</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end breadcrumb section -->
+		
+		<!-- error section -->
+		<div class="full-height-section error-section">
+			<div class="full-height-tablecell">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-8 offset-lg-2 text-center">
+							<div class="error-text">
+								<i class="far fa-sad-cry"></i>
+								<h1>Thank You For Registering!</h1>
+								<p>Have Fun Shopping at L'huile Naturelle !</p>
+								<a href="index.html" class="boxed-btn">Back to Home</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end error section -->
+		
+		<!-- footer -->
+	<div class="footer-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box about-widget">
+						<h2 class="widget-title">About us</h2>
+						<p>L'huile Naturelle pioneered the spa industry in Bacolod City conforming to high standards of excellence.</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box get-in-touch">
+						<h2 class="widget-title">Get in Touch</h2>
+						<ul>
+							<li>Barangay Villamonte, Bacolod City</li>
+							<li>support@LhuileNaturelle.com</li>
+							<li>+63 956 165 9469</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box pages">
+						<h2 class="widget-title">Pages</h2>
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="shop-pg1.html">Shop</a></li>
+							<li><a href="register.html">Register</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<div class="footer-box subscribe">
+						<h2 class="widget-title">Subscribe</h2>
+						<p>Subscribe to our mailing list to get the latest updates.</p>
+						<form action="index.html">
+							<input type="email" placeholder="Email">
+							<button type="submit"><i class="fas fa-paper-plane"></i></button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end footer -->
+	
+	<!-- copyright -->
+	<div class="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-12">
+					<p>Copyrights &copy; 2023 - <a href="">Emdee</a>,  All Rights Reserved.<br>
+					</p>
+				</div>
+				<div class="col-lg-6 text-right col-md-12">
+					<div class="social-icons">
+						<ul>
+							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
+							<li><a href="#" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+							<li><a href="#" target="_blank"><i class="fab fa-dribbble"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end copyright -->
+	
+	<!-- jquery -->
+	<script src="assets/js/jquery-1.11.3.min.js"></script>
+	<!-- bootstrap -->
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<!-- count down -->
+	<script src="assets/js/jquery.countdown.js"></script>
+	<!-- isotope -->
+	<script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
+	<!-- waypoints -->
+	<script src="assets/js/waypoints.js"></script>
+	<!-- owl carousel -->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!-- magnific popup -->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- mean menu -->
+	<script src="assets/js/jquery.meanmenu.min.js"></script>
+	<!-- sticker js -->
+	<script src="assets/js/sticker.js"></script>
+	<!-- main js -->
+	<script src="assets/js/main.js"></script>
+	
+	</body>
+</html>
